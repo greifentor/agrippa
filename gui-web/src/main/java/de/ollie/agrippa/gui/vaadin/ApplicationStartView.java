50dd2f89b9dfd6ae578a73276f8c8a0e29b3d79a
ollie <lieshoff@gmx.de>
2024-06-30 20:16:08 +0200

    Made application start view generated not any longer.

--- gui-web/src/main/java/de/ollie/agrippa/gui/vaadin/ApplicationStartView.java
+++ gui-web/src/main/java/de/ollie/agrippa/gui/vaadin/ApplicationStartView.java
@@ -22,8 +22,6 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * A start view for the application.
- *
- * GENERATED CODE !!! DO NOT CHANGE !!!
  */
 @Route(ApplicationStartView.URL)
 @PreserveOnRefresh
