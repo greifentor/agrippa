67225326f49e7161a14437a09b32e5ce17512643
ollie <lieshoff@gmx.de>
2024-06-30 23:37:32 +0200

    Re generated after generator updated.

--- gui-web/src/main/java/de/ollie/agrippa/gui/vaadin/masterdata/TaskMaintenanceView.java
+++ gui-web/src/main/java/de/ollie/agrippa/gui/vaadin/masterdata/TaskMaintenanceView.java
@@ -12,6 +12,7 @@ import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.Route;
 
 import de.ollie.agrippa.core.model.Task;
+import de.ollie.agrippa.core.service.ProjectService;
 import de.ollie.agrippa.core.service.localization.ResourceManager;
 import de.ollie.agrippa.gui.SessionData;
 import de.ollie.agrippa.gui.vaadin.UserAuthorizationChecker;
@@ -76,10 +77,9 @@ public class TaskMaintenanceView extends AbstractMasterDataBaseLayout implements
 
 	@Override
 	public void doSetParameter(BeforeEvent event) {
-		long id =
-				parametersMap.containsKey("id") && (parametersMap.get("id").size() > 0)
-						? Long.parseLong(parametersMap.get("id").get(0))
-						: -1;
+		long id = parametersMap.containsKey("id") && (parametersMap.get("id").size() > 0)
+				? Long.parseLong(parametersMap.get("id").get(0))
+				: -1;
 		model = serviceProvider.getTaskService().findById(id).orElse(createNewModel());
 		if (parametersMap.containsKey("duplicate") && "true".equals(parametersMap.get("duplicate").get(0))) {
 			model.setId(-1);
@@ -104,32 +104,26 @@ public class TaskMaintenanceView extends AbstractMasterDataBaseLayout implements
 		getStyle().set("background-attachment", "fixed");
 		add(
 				new HeaderLayout(
-						buttonFactory.createBackButton(resourceManager, this::getUI, TaskPageView.URL, session),
+						buttonFactory
+										.createBackButton(
+												resourceManager,
+												this::getUI,
+												TaskPageView.URL,
+												session),
 						buttonFactory.createLogoutButton(resourceManager, this::getUI, session, logger),
-						resourceManager
-								.getLocalizedString(
-										"TaskMaintenanceView.header.prefix.label",
-										session.getLocalization())
-								+ getHeaderSuffix(model),
-						HeaderLayoutMode.PLAIN),
+								resourceManager.getLocalizedString("TaskMaintenanceView.header.prefix.label", session.getLocalization()) + getHeaderSuffix(model),
+								HeaderLayoutMode.PLAIN),
 				getDetailsLayout(model));
 	}
 
 	private String getHeaderSuffix(Task model) {
-		return maintenanceViewRenderer != null ? maintenanceViewRenderer.getHeaderSuffix(model) : "" + model.getTitle();
+		return maintenanceViewRenderer != null
+				? maintenanceViewRenderer.getHeaderSuffix(model)
+				: "" + model.getTitle();
 	}
 
 	private AbstractMasterDataBaseLayout getDetailsLayout(Task model) {
-		return new TaskDetailsLayout(
-				buttonFactory,
-				componentFactory,
-				model,
-				serviceProvider,
-				guiConfiguration,
-				resourceManager,
-				session,
-				this,
-				comboBoxItemLabelGenerator);
+		return new TaskDetailsLayout(buttonFactory, componentFactory, model, serviceProvider, guiConfiguration, resourceManager, session, this, comboBoxItemLabelGenerator);
 	}
 
 	@Override
