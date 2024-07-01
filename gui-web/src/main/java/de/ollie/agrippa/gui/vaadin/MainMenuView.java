f2bc87365ffbfef7aa6a89779e1398358bb70c91
ollie <lieshoff@gmx.de>
2024-07-01 23:16:38 +0200

    Added a todo list to the menu view.

--- gui-web/src/main/java/de/ollie/agrippa/gui/vaadin/MainMenuView.java
+++ gui-web/src/main/java/de/ollie/agrippa/gui/vaadin/MainMenuView.java
@@ -3,6 +3,9 @@ package de.ollie.agrippa.gui.vaadin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
@@ -12,6 +15,11 @@ import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.OptionalParameter;
 import com.vaadin.flow.router.Route;
 
+import de.ollie.agrippa.core.model.Task;
+import de.ollie.agrippa.core.model.TaskStatus;
+import de.ollie.agrippa.core.model.Todo;
+import de.ollie.agrippa.core.model.TodoStatus;
+import de.ollie.agrippa.core.service.TaskService;
 import de.ollie.agrippa.core.service.localization.ResourceManager;
 import de.ollie.agrippa.gui.SessionData;
 import de.ollie.agrippa.gui.vaadin.component.Button;
@@ -39,6 +47,7 @@ public class MainMenuView extends Scroller implements BeforeEnterObserver, HasUr
 	private final GUIConfiguration guiConfiguration;
 	private final ResourceManager resourceManager;
 	private final SessionData session;
+	private final TaskService taskService;
 	private final WebAppConfiguration configuration;
 
 	@Override
@@ -66,6 +75,7 @@ public class MainMenuView extends Scroller implements BeforeEnterObserver, HasUr
 		ButtonGrid buttonGridMasterData = new ButtonGrid(4, buttonMasterData);
 		buttonGridMasterData.setMargin(false);
 		buttonGridMasterData.setWidthFull();
+		Component todoOverviewLayout = createTodoOverviewLayout();
 		VerticalLayout mainLayout = new VerticalLayout();
 		mainLayout
 				.add(
@@ -75,9 +85,57 @@ public class MainMenuView extends Scroller implements BeforeEnterObserver, HasUr
 										.getLocalizedString("commons.header.main-menu.label", session.getLocalization())
 										.replace("{0}", configuration.getAppVersion()),
 								HeaderLayoutMode.PLAIN),
-						buttonGridMasterData);
+						buttonGridMasterData,
+						todoOverviewLayout);
 		setContent(mainLayout);
 		LOG.info("main menu view opened for user '{}'.", session.getUserName());
+	}
+
+	private Component createTodoOverviewLayout() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.getStyle().set("-moz-border-radius", "4px");
+		layout.getStyle().set("-webkit-border-radius", "4px");
+		layout.getStyle().set("border-radius", "4px");
+		layout.getStyle().set("border", "1px solid gray");
+		layout
+				.getStyle()
+				.set(
+						"box-shadow",
+						"10px 10px 20px #e4e4e4, -10px 10px 20px #e4e4e4, -10px -10px 20px #e4e4e4, 10px -10px 20px #e4e4e4");
+		layout.setMargin(false);
+		layout.setPadding(true);
+		layout.setSizeFull();
+		taskService
+				.findAll()
+				.stream()
+				.filter(t -> t.getTaskStatus() != TaskStatus.SOLVED)
+				.forEach(t -> addTodo(layout, t));
+		return layout;
+	}
+
+	private void addTodo(VerticalLayout layout, Task task) {
+		task
+				.getTodos()
+				.stream()
+				.filter(t -> t.getStatus() != TodoStatus.SOLVED)
+				.forEach(t -> layout.add(createTodoLayout(task, t)));
+	}
+
+	private Component createTodoLayout(Task task, Todo todo) {
+		HorizontalLayout layout = new HorizontalLayout();
+		layout.setMargin(false);
+		layout.setPadding(false);
+		layout.setWidthFull();
+		layout.add(createLabel(todo.getStatus().name(), "5%"));
+		layout.add(createLabel(task.getTitle(), "35%"));
+		layout.add(createLabel(todo.getTitle(), "60%"));
+		return layout;
+	}
+
+	private Component createLabel(String text, String width) {
+		Label label = new Label(text);
+		label.setWidth(width);
+		return label;
 	}
 
 	private void switchToMasterData() {
