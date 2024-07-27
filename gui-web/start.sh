0ffeee8698063dd1418adae473936ee7ff9e0bb7
ollie <lieshoff@gmx.de>
2024-07-28 00:45:49 +0200

    Increased version number.

--- gui-web/start.sh
+++ gui-web/start.sh
@@ -1,1 +1,1 @@
-java -Dserver.port=8080 -Dcube.disabled.user.name=OTFLZT-0000000001 -Dcube.enabled=false -Dcube.url=http://localhost:8080/cube -jar target/agrippa-gui-0.0.1.jar
+java -Dserver.port=8080 -Dcube.disabled.user.name=OTFLZT-0000000001 -Dcube.enabled=false -Dcube.url=http://localhost:8080/cube -jar target/agrippa-gui-1.1.0.jar
