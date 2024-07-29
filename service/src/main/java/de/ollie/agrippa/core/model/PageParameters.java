aee0a5683336ead133778b245eaba040527234dd
ollie <lieshoff@gmx.de>
2024-07-29 22:48:49 +0200

    Update after code generator modification.

--- service/src/main/java/de/ollie/agrippa/core/model/PageParameters.java
+++ service/src/main/java/de/ollie/agrippa/core/model/PageParameters.java
@@ -14,7 +14,20 @@ import lombok.experimental.Accessors;
 @Generated
 public class PageParameters {
 
+	public enum Direction {
+		ASC,
+		DESC;
+	}
+
+	@Accessors(chain = true)
+	@Data
+	public static class Sort {
+		private Direction direction;
+		private String[] fieldNames;
+	}
+
 	private int pageNumber;
 	private int entriesPerPage;
+	private Sort sort;
 
 }
--- service/src/main/java/de/ollie/agrippa/persistence/converter/PageParametersToPageableConverter.java
+++ service/src/main/java/de/ollie/agrippa/persistence/converter/PageParametersToPageableConverter.java
@@ -4,6 +4,7 @@ import javax.inject.Named;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 
 import de.ollie.agrippa.core.model.PageParameters;
 
@@ -22,7 +23,19 @@ public class PageParametersToPageableConverter {
 		if (pageParameters == null) {
 			return null;
 		}
+		if (pageParameters.getSort() != null) {
+			return PageRequest
+					.of(
+							pageParameters.getPageNumber(),
+							pageParameters.getEntriesPerPage(),
+							getDirection(pageParameters.getSort().getDirection()),
+							pageParameters.getSort().getFieldNames());
+		}
 		return PageRequest.of(pageParameters.getPageNumber(), pageParameters.getEntriesPerPage());
+	}
+
+	private Direction getDirection(PageParameters.Direction direction) {
+		return direction == PageParameters.Direction.ASC ? Direction.ASC : Direction.DESC;
 	}
 
 }
