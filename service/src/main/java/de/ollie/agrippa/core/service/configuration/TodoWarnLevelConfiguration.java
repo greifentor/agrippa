d8cbf2a1b70f69e9b6dfc115409702631dfc08d3
oliver.lieshoff <oliver.lieshoff@host.docker.internal>
2024-11-06 10:53:42 +0100

    Configuration corrected.

--- service/src/main/java/de/ollie/agrippa/core/service/configuration/TodoWarnLevelConfiguration.java
+++ service/src/main/java/de/ollie/agrippa/core/service/configuration/TodoWarnLevelConfiguration.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TodoWarnLevelConfiguration {
 
-    @Value("${warn.level1.offset.minutes:3360}") // 56 h
+    @Value("${warn.level1.offset.minutes:3410}") // 56 h
 	private int warnLevel1OffsetMinutes;
 
-    @Value("${warn.level2.offset.minutes:1920}") // 32 h
+    @Value("${warn.level2.offset.minutes:1980}") // 32 h
 	private int warnLevel2OffsetMinutes;
 
-	@Value("${warn.level3.offset.minutes:480}") // 8 h
+    @Value("${warn.level3.offset.minutes:540}") // 8 h
 	private int warnLevel3OffsetMinutes;
 
 	public int getWarnLevel1OffsetInMinutes() {
